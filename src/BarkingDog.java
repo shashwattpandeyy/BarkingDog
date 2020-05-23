a4fb68af764904227a4c545b912780771d65f1d1
shashwatpandey1 <Shashwat.pandey_cs18@gla.ac.in>
2020-05-24 00:59:53 +0530

    Create method Barking dog this method helps to bark dog according to time.

---
+++ src/BarkingDog.java
@@ -0,0 +1,11 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking , int hourOfDay){
+        if (hourOfDay <8 || hourOfDay >22){
+            return barking;
+        }else if(hourOfDay<0 || hourOfDay>23){
+            return false;
+        }else
+            return false;
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        boolean t = BarkingDog.shouldWakeUp ( true, 7 );
+        System.out.println (t);
+    }
+}
